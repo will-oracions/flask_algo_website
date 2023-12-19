7f730ca1c50fc6e0de639c4926f19b0d2c082e65
will-oracions <oracions.dev@gmail.com>
2023-12-19 20:57:22 +0100

    exercice 17

--- algorithms.py
+++ algorithms.py
@@ -1,4 +1,4 @@
-# algorithms.py
+# factoriel
 def calculate_factorial(n):
     if n < 0:
         raise ValueError("Veuillez entrer un nombre entier positif.")
@@ -6,7 +6,7 @@ def calculate_factorial(n):
         return 1
     else:
         return n * calculate_factorial(n - 1)
-
+# est premier
 def is_prime(n):
     if n < 2:
         return False
@@ -15,9 +15,19 @@ def is_prime(n):
             return False
     return True
 
+# Copains
 def are_amicable_numbers(p, q):
     return sum_of_divisors(p) == q and sum_of_divisors(q) == p
 
 def sum_of_divisors(n):
     return sum(i for i in range(1, n) if n % i == 0)
 
+# schtroumpf
+def calculate_schtroumpf(array_a, array_b):
+    elements_a = [int(x.strip()) for x in array_a.split(',')]
+    elements_b = [int(x.strip()) for x in array_b.split(',')]
+
+    if len(elements_a) != len(elements_b):
+        raise ValueError("Les tableaux doivent avoir la même longueur.")
+
+    return sum(a * b for a, b in zip(elements_a, elements_b))
--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request
-from algorithms import calculate_factorial, is_prime, are_amicable_numbers
+from algorithms import calculate_factorial, is_prime, are_amicable_numbers, calculate_schtroumpf
 
 app = Flask(__name__)
 
@@ -38,10 +38,8 @@ def exercice(num):
     if request.method == 'POST':
         try:
             result = calculate_exercice(num, request.form)
-        except ValueError:
-            errorMessage = 'Veuillez entrer un nombre entier positif.'
-
-        print(errorMessage)
+        except ValueError as error:
+            errorMessage = str(error)
     return render_template(f'exercice.html', errorMessage=errorMessage, input=request.form, num=str(num), result=result, exercises=exercises)
 
 def calculate_exercice(num, form):
@@ -57,6 +55,11 @@ def calculate_exercice(num, form):
         p = int(form['numberP'])
         q = int(form['numberQ'])
         return are_amicable_numbers(p, q)
+    
+    if num == 17:
+        arr1 = form['arrayA']
+        arr2 = form['arrayB']
+        return calculate_schtroumpf(arr1, arr2)
     # Ajoutez d'autres calculs d'exercices en important les fonctions nécessaires depuis algorithms.py
     return None
 
